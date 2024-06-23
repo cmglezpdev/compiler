da276a902ae1daae543a7bcd0854cb9106108aae
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-06-23 13:42:49 -0400

    feature(hulk): add principal constants

---
+++ src/hulk/constants.py
@@ -0,0 +1,16 @@
+SPECIAL_TOKENS = [";", ",", ".", ":", "(", ")", "[", "]", "{", "}",
+                  "<", ">", "=", "+", "-", "*", "/", "^", "**", "%", "@",
+                  "@@", "!", ">=", "<=", "==", "!=", "&", ":=", "=>", "||", "|"]
+
+RESERVED_WORDS = ["let", "for", "while",
+                  "if", "elif", "else", "function", "in", "is", "as", "new", "type", "protocol", "inherits", "extends"]
+
+DEFINED_FUNCTIONS = ["sqrt", "sin", "cos",
+                     "tan", "cot", "print", "exp", "log", "rand", "range"]
+
+NUMERIC_CONSTANTS = ["PI", "E"]
+
+NUMBER = 'NUMBER'
+IDENTIFIER = 'IDENTIFIER'
+STRING = 'STRING'
+BOOLEAN = 'BOOLEAN'
