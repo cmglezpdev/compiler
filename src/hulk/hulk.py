65cee245e644da34f7d90ee0b50b635f7672b0da
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-07-01 22:13:20 -0400

    feature(hulk): add main file to execute hulk

---
+++ src/hulk/hulk.py
@@ -0,0 +1,52 @@
+from hulk.hulk_semantic_check import hulk_semantic_check
+from .lexer import hulk_lexer_build
+from .parser import hulk_parser_build, hulk_to_grammar, hulk_parse
+from tools.lexer import Lexer
+from .hulk_grammar import hulk_grammar
+from .code_gen import hulk_code_generator
+import subprocess
+
+
+def hulk_build() -> bool:
+    hulk_lexer_build()
+    return hulk_parser_build()
+
+
+def hulk_compile_str(program: str) -> bool:
+    hulk_lexer = Lexer()
+    hulk_lexer.load('hulk')
+
+    result = hulk_lexer.run(program)
+    tokens = result.tokens
+
+    if not result.ok:
+        print(
+            f'Lexer error:\n{result.error.row+1}:{result.error.col+1}')
+        return False
+
+    result = hulk_parse([hulk_to_grammar(t) for t in result.tokens])
+
+    if not result.ok:
+        print(
+            f'Parser error:\n{tokens[result.error-1].row+1}:{tokens[result.error-1].col+1}')
+        return False
+
+    ast = hulk_grammar.evaluate(result.derivation_tree, tokens)
+    result = hulk_semantic_check(ast)
+
+    if not result.ok:
+        error = '\n'.join(result.errors)
+        print(f'Semantic errors:\n{error}')
+        return False
+
+    hulk_code_generator(ast, result.context)
+
+    try:
+        result = subprocess.run(
+            ["gcc", "-o", "cache/main", "cache/main.c", "-lm"])
+        result = subprocess.run(
+            ["./cache/main"], text=True, check=True)
+    except:
+        print('Runtime error')
+
+    return True
